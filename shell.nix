7a04adae5cdf78fdae68afc139a6f545ab02769f
Bob Long <robertjflong@gmail.com>
2015-12-12 21:10:48 +0000

    bump

--- shell.nix
+++ shell.nix
@@ -10,7 +10,7 @@ let
       }:
       mkDerivation {
         pname = "yesod-csp";
-        version = "0.1.1.0";
+        version = "0.2.0.0";
         src = ./.;
         libraryHaskellDepends = [
           attoparsec base mono-traversable network-uri semigroups syb
